15d06ec5c9729175060817ceac702cf07c618948
Fabian <fabiorellana95@gmail.com>
2019-04-12 03:40:16 -0400

    Construyendo la aplicacion

---
+++ src/index.js
@@ -0,0 +1,103 @@
+const {	app, BrowserWindow, Menu } = require('electron');
+const url = require('url');
+const path = require('path');
+
+
+if (process.env.NODE_ENV !==  'production') {
+	require('electron-reload')(__dirname, {
+		electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+	})
+}
+
+
+let mainWindow
+let newProductWindow
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({});
+	mainWindow.loadURL(url.format({
+		//Cargar el archivo principal
+		pathname: path.join(__dirname, 'views/index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+
+	const mainMenu = Menu.buildFromTemplate(templateMenu);
+	Menu.setApplicationMenu(mainMenu);
+
+	mainWindow.on('closed', () => {
+		app.quit();
+	});
+
+});
+
+function createNewProductWindow() {
+	newProductWindow = new BrowserWindow({
+		width: 400,
+		height: 330,
+		title: 'Add A New Product'
+	});
+
+	newProductWindow.setMenu(null);
+	newProductWindow.loadURL(url.format({
+		//Cargar el archivo principal
+		pathname: path.join(__dirname, 'views/new-product.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+
+	newProductWindow.on('closed', () => {
+		newProductWindow = null;
+	});
+}
+
+const templateMenu = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'New Product',
+				accelerator: 'Ctrl+N',
+				click() {
+					createNewProductWindow();
+				}
+			},
+			{
+				label: 'Remove All Products',
+				click() {
+
+				}
+			},
+			{
+				label: 'Exit',
+				accelerator: process.platform == 'darwin' ? 'command+Q' : 'Ctrl+Q',
+				click() {
+					app.quit();
+				}
+			}
+		]
+	}
+];
+
+if (process.platform === 'darwin') {
+	templateMenu.unshift({
+		label: app.getName()
+	});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+	templateMenu.push({
+		label: 'DevTools',
+		submenu: [
+			{
+				label: 'Show/Hide Dev Tools',
+				click(item, focusetWindow) {
+					focusetWindow.toggleDevTools();
+				}
+			},
+			{
+				role: 'reload'
+			}
+		]
+	})
+}
